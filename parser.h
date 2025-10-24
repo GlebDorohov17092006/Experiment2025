ca391858312c9f0176b2c8dd028728e224738569
SageXPhoenix <timofeev.ar@phystech.edu>
2025-10-24 12:10:25 +0300

    Added general parser consisting of parsers of json and csv files

--- parser.h
+++ parser.h
@@ -4,6 +4,12 @@
 #include <vector>
 #include "variable.h"
 
-void parser_json(std::vector<Variable> &variables);
+//Parser for json file
+void parser_json(std::vector<Variable> &variables, const std::string& filename);
 
+//Parser for csv file
 std::vector<Variable> parser_csv(const std::string& filename);
+
+//General parser
+std::vector<Variable> parser(const std::string& filename);
+
