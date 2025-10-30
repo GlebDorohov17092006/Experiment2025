32ee6815a0985d28327e1e8d2d8fbf19b15f7653
GlebDorohov17092006 <dorokhov.gv@phystech.edu>
2025-10-30 19:16:07 +0500

    I fixed bugs and improved readability.

--- AbsoluteInstrument.cpp
+++ AbsoluteInstrument.cpp
@@ -7,10 +7,13 @@ AbsoluteInstrument::AbsoluteInstrument(const std::string &name, double error)
 
 double AbsoluteInstrument::get_error(size_t index, double value) const
 {
+    (void)index;
+    (void)value;
     return error;
 }
 
 void AbsoluteInstrument::set_error(size_t index, double error)
 {
+    (void)index;
     this->error = error;
 }
