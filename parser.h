889938b81d31dc86affb4c405610c169c9e0bab0
SageXPhoenix <timofeev.ar@phystech.edu>
2025-10-31 04:37:34 +0300

    Added functionality of action Import files

--- parser.h
+++ parser.h
@@ -2,13 +2,7 @@
 
 #include <string>
 #include <vector>
-#include "variable.h"
-
-//Parser for json file
-void parser_json(std::vector<Variable> &variables, const std::string& filename);
-
-//Parser for csv file
-std::vector<Variable> parser_csv(const std::string& filename);
+#include "Variable.h"
 
 //General parser
 std::vector<Variable> parser(const std::string& filename_csv, const std::string& filename_json);
